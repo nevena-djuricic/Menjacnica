c8a45ad3539eafdfae9792df0529c1f3c9853f52
nevena-djuricic <dj.nevena@hotmail.com>
2015-03-31 11:33:16 +0200

    Dodata klasa Menjacnica koja implementira interfejs MenjacnicaInterfejs

---
+++ src/menjacnica/Menjacnica.java
@@ -0,0 +1,26 @@
+package menjacnica;
+
+import java.util.GregorianCalendar;
+import java.util.LinkedList;
+
+import menjacnica.interfejs.MenjacnicaInterfejs;
+
+public class Menjacnica implements MenjacnicaInterfejs {
+
+	public void dodajKurs(Valuta kurs) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void izbrisiKurs(Valuta kurs) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public LinkedList<Valuta> pronadjiKurs(String valuta,
+			GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
